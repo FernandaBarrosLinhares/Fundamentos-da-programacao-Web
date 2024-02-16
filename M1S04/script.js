9877a193bfb98719f16124a74b2810793b00c5df
Fernanda Barros Linhares <fernanda_linhares@estudante.sesisenai.org.br>
2024-02-16 20:16:06 -0300

    Exercicio 3

--- M1S04/script.js
+++ M1S04/script.js
@@ -1,12 +1,29 @@
-function parOuImpar(numero) {
-    if (numero % 2 === 0) {
-        return "O número " + numero + " é par.";
-    } else {
-        return "O número " + numero + " é ímpar.";
-    }
-}
+//Exercicio 2
 
-// Exemplo de uso da função
-console.log(parOuImpar(7)); // Saída: O número 7 é ímpar.
-console.log(parOuImpar(10)); // Saída: O número 10 é par.
-console.log(parOuImpar(13)); // Saída: O número 13
+// function parOuImpar(numero) {
+//     if (numero % 2 === 0) {
+//         return "O número " + numero + " é par.";
+//     } else {
+//         return "O número " + numero + " é ímpar.";
+//     }
+// }
+
+// // Exemplo de uso da função
+// console.log(parOuImpar(7)); // Saída: O número 7 é ímpar.
+// console.log(parOuImpar(10)); // Saída: O número 10 é par.
+// console.log(parOuImpar(13)); // Saída: O número 13
+
+
+//Exercicio 3
+
+const exercicio = (array)=> {
+    let soma = 0;
+    for(let numero of array){
+        soma += numero;
+    }
+    let media = soma/array.length;
+    return media;
+}
+let arrayNumeros = [10,20,30];
+console.log (exercicio(arrayNumeros));
+
