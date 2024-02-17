be10fe18d8a79ff8838903be1722e51d7f2ac77c
Fernanda Barros Linhares <fernanda_linhares@estudante.sesisenai.org.br>
2024-02-16 23:40:36 -0300

    Exercicio 7

--- M1S04/script.js
+++ M1S04/script.js
@@ -29,15 +29,15 @@
 
 //Exercicio 4
 
-function adicao(numero1, numero2,funcaoAnonima){
-    let soma = numero1 + numero2;
+// function adicao(numero1, numero2,funcaoAnonima){
+//     let soma = numero1 + numero2;
 
-    funcaoAnonima(soma);
-}
+//     funcaoAnonima(soma);
+// }
 
-adicao(2,2, (sum)=>{
-    console.log("O resultado final é:" + sum);
-})
+// adicao(2,2, (sum)=>{
+//     console.log("O resultado final é:" + sum);
+// })
 
 //Exercicio 5
 
@@ -58,9 +58,45 @@ adicao(2,2, (sum)=>{
 
 //Exercicio 6
 
-for (let i = 0; i <= 10; i++) {
-    console.log(`5 x ${i} = ${5 * i}`);
+// for (let i = 0; i <= 10; i++) {
+//     console.log(`5 x ${i} = ${5 * i}`);
+// }
+
+//Exercicio 7
+
+function compra(){
+    let produto, qtde, index
+    let  lista = [0,0,0,0,0]
+    let produtos =['Hortifruti','Laticínios','Carnes','Peixes', 'Aves']
+    let maiorQtde = 0
+
+    while (true) {
+        produto = prompt('Qual produto você deseja comprar?,(1)Hortifruti,(2)Laticínios,(3)Carnes,(4)Peixes,(5)Aves(6)Fechar pedido')
+
+        if (produto == 1 || produto == 2 || produto == 3 || produto == 4 || produto == 5) {
+            qtde = parseInt(prompt('Qual a quantidade deste item?'));
+            lista[parseInt(produto) -1] +=qtde;
+
+        } else if (produto==6){
+            break;
+        } else {
+            alert("Opção inválida!");
+        }
+
+    }
+    for(let i =0 ; i< lista.length; i++){
+        if(lista[i] > maiorQtde){
+            maiorQtde = lista[i];
+            index = i;
+        }
+    }
+
+return document.write('Seu maior pedido foi de ${produtos[index]} com ${maiorQtde} itens.')
 }
+
+compra()
+
+
 
 
 
